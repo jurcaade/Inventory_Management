afc65a4a2eaaee99df807ad16de5ebc05893320d
jurcaade <jurcaadelina11@gmai.com>
2025-04-22 15:57:14 +0300

    Add attributes and methods to Product and Supplier classes

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,76 @@
 package model;
 
 public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantityInStock;
+    private String category;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, String description, double price, int quantityInStock, String category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantityInStock = quantityInStock;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) throw new IllegalArgumentException("Price cannot be negative!");
+        this.price = price;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", name=" + name + ", price=" + price + ", stock=" + quantityInStock + "]";
+    }
 }
--- src/main/java/model/Supplier.java
+++ src/main/java/model/Supplier.java
@@ -1,4 +1,55 @@
 package model;
 
 public class Supplier {
+    private int supplierId;
+    private String name;
+    private String contactInfo;
+    private String address;
+
+    public Supplier() {
+    }
+
+    public Supplier(int supplierId, String name, String contactInfo, String address) {
+        this.supplierId = supplierId;
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.address = address;
+    }
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier [id=" + supplierId + ", name=" + name + ", contact=" + contactInfo + "]";
+    }
 }
